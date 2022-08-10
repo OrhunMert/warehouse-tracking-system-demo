117b9c7d1de825f93cebc53d9582ef1d3b7ac03e
Orhun Mert Bozkurt <70032540+OrhunMert@users.noreply.github.com>
2022-08-10 14:51:43 +0300

    add the swagger in warehouse service

--- warehouse/src/main/java/com/trackingsystem/warehouse/config/SwaggerConfig.java
+++ warehouse/src/main/java/com/trackingsystem/warehouse/config/SwaggerConfig.java
@@ -1,27 +1,30 @@
-package com.trackingsystem.user.config;
+package com.trackingsystem.warehouse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
+
 import springfox.documentation.spi.DocumentationType;
+
 import springfox.documentation.spring.web.plugins.Docket;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
     @Bean
-    public Docket getUserApi() {
+    public Docket getWarehouseApi(){
         return new Docket(DocumentationType.SWAGGER_2).
                 select().
-                apis(RequestHandlerSelectors.basePackage("com.trackingsystem.user.controller")).
+                apis(RequestHandlerSelectors.any()).
                 build();
     }
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry)
-    {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
--- warehouse/src/main/java/com/trackingsystem/warehouse/controller/WarehouseController.java
+++ warehouse/src/main/java/com/trackingsystem/warehouse/controller/WarehouseController.java
@@ -5,51 +5,59 @@ import com.trackingsystem.warehouse.dto.UpdatedWarehouseDto;
 import com.trackingsystem.warehouse.dto.WarehouseDto;
 import com.trackingsystem.warehouse.service.WarehouseService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.Valid;
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/warehouses")
+@Api(value = "Warehouse's Api")
 public class WarehouseController {
     private final WarehouseService warehouseService;
     @PostMapping()
+    @ApiOperation(value = "Create Warehouse")
     ResponseEntity<WarehouseDto> createWarehouse(@Valid @RequestBody WarehouseDto warehouseDTO){
         return ResponseEntity.ok().body(warehouseService.createWarehouse(warehouseDTO));
     }
     @PostMapping("/buy")
+    @ApiOperation(value = "Buy Product in Warehouse")
     ResponseEntity<WarehouseOperationDto> buyProductForWarehouse(@RequestParam Long id,
                                                                  @RequestParam String productName){
         return ResponseEntity.ok().body(warehouseService.buyProduct(id,productName));
     }
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get Warehouse")
     ResponseEntity<WarehouseDto> getWarehouse(@PathVariable Long id){
         return ResponseEntity.ok().body(warehouseService.getWarehouse(id));
     }
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update Warehouse")
     ResponseEntity<UpdatedWarehouseDto> updateWarehouse(@PathVariable Long id,
                                                         @Valid @RequestBody UpdatedWarehouseDto updateWarehouseDTO){
         return ResponseEntity.ok().body(warehouseService.updateWarehouse(id,updateWarehouseDTO));
     }
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete Warehouse")
     ResponseEntity<Object> deleteWarehouse(@PathVariable Long id){
         warehouseService.deleteWarehouse(id);
         return ResponseEntity.ok().build();
     }
     @DeleteMapping("/sell")
+    @ApiOperation(value = "Sell Product in Warehouse")
     ResponseEntity<WarehouseOperationDto> sellProductForWarehouse(@RequestParam Long id,
                                                       @RequestParam String productName){
         return ResponseEntity.ok().body(warehouseService.sellProduct(id,productName));
